0357ca13b0ce201d9479e84977aa7c886b4e0e55
Siri Sirironnachai <warinphon6999@gmail.com>
2024-12-24 14:58:56 +0700

    Add date

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+const dateDisplay = document.getElementById('date');
+const currentDate = new Date();
+const options = {year: 'numeric', month: 'long', day: 'numeric' };
+dateDisplay.textContent = currentDate.toLocaleDateString('en-US', options);
 
 setInterval(function() {
     const date = new Date()
     hours.textContent = date.getHours()
     minutes.textContent = date.getMinutes()
     seconds.textContent = date.getSeconds()
-})
+    dateDisplay.textContent = date.toLocaleDateString()
+}, 1000)
